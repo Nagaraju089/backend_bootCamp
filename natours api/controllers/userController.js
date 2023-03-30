cb9a489d2e83dfa986183ad52619ed3478897d37
nagaraju <nagarajuambati089@gmail.com>
2023-03-31 01:28:46 +0530

    third commit

--- natours api/controllers/userController.js
+++
@@ -1,3 +0,0 @@
-const User = require('./../models/userModel');
-const catchAsync = require('./../utils/catchAsync');
-
--- natours api/utils/catchAsync.js
+++
@@ -1,5 +0,0 @@
-module.exports = fn => {
-    return(req, res, next) => { 
-    fn(req, res, next).catch(next); //if error occurs while creating, then that error will be handled in error handling middeware
-    }
-}
